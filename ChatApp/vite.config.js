3130d32d99efdf527bb6755bc88c03fa0d4327ae
Omar Mabrouk <omersa2002@gmail.com>
2025-01-15 00:46:41 +0200

    basec chat

--- ChatApp/vite.config.js
+++ ChatApp/vite.config.js
@@ -4,10 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     proxy: {
         "/socket.io": {
-          target: "http://localhost:3000",
+          //target: "http://localhost:3000",
+          target:"https://chat-app-server-virid.vercel.app",
           changeOrigin: true,
           ws: true, // Enable WebSocket proxying
         },
